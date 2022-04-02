50fe75b1bb8031c3602a226c0692949c98238f19
rob <robyen22@gmail.com>
2022-04-02 13:59:14 +0200

    Implement BitBag Coding Standard

--- ecs.php
+++ ecs.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -13,4 +15,12 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     ]);
 
     $containerConfigurator->services()->set(BinaryOperatorSpacesFixer::class)->call('configure', [[]]);
+
+    $containerConfigurator->import('vendor/bitbag/coding-standard/ecs.php');
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
 };
